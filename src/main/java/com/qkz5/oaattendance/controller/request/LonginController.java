03b55236dd68c0cb0e57ff997427b1c707f9c6b4
wdabao <723157193@qq.com>
2018-11-01 11:34:49 +0800

    登陸届滿

--- src/main/java/com/qkz5/oaattendance/controller/request/LonginController.java
+++ src/main/java/com/qkz5/oaattendance/controller/request/LonginController.java
@@ -1,5 +1,8 @@
 package com.qkz5.oaattendance.controller.request;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -22,11 +25,28 @@ import javax.servlet.http.HttpServletRequest;
 public class LonginController {
     private static Logger LOG = LoggerFactory.getLogger(LonginController.class);
     @RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
-    @ResponseBody
     public String login(@PathVariable String id , Model model, HttpServletRequest request) {
+        Subject currentUser = SecurityUtils.getSubject();
+        if (!currentUser.isAuthenticated()) {
+            // 把用户名和密码封装为 UsernamePasswordToken 对象
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            // rememberme
+            token.setRememberMe(true);
+            try {
+                System.out.println("1. " + token.hashCode());
+                // 执行登录.
+                currentUser.login(token);
+            }
+            // ... catch more exceptions here (maybe custom ones specific to your application?
+            // 所有认证时异常的父类.
+            catch (AuthenticationException ae) {
+                //unexpected condition?  error?
+                System.out.println("登录失败: " + ae.getMessage());
+            }
+        }
 
 
-        return "跳转的页面路径";
+        return "dengluhou";
     }
 
 }
