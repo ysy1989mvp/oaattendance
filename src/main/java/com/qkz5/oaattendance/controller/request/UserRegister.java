471f1a36e452afc52e784e90fa46011740124f6e
slkj caidingnu <kmslkj5@192.168.4.103>
2018-11-01 11:16:16 +0800

    3

---
+++ src/main/java/com/qkz5/oaattendance/controller/request/UserRegister.java
@@ -0,0 +1,19 @@
+package com.qkz5.oaattendance.controller.request;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Author: caidingnu
+ * Date:   2018/11/1
+ * Time：  11:09
+ * Content:
+ */
+
+@Api(value = "员工信息录入")
+@Controller
+public class UserRegister {
+
+
+
+}
