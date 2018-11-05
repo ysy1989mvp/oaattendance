f03fbced7d65001083c62fd4cd4a93fb2f6d4d4f
wdabao <723157193@qq.com>
2018-11-05 16:13:40 +0800

    登陸届滿

--- src/main/java/com/qkz5/oaattendance/config/SessionConfig.java
+++ src/main/java/com/qkz5/oaattendance/config/SessionConfig.java
@@ -1,11 +1,11 @@
-package com.qkz5.oaattendance.config;
-			
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.ConfigureRedisAction;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
-public class SessionConfig {
-}
+//package com.qkz5.oaattendance.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.session.data.redis.config.ConfigureRedisAction;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//
+//@Configuration
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
+//public class SessionConfig {
+//}
